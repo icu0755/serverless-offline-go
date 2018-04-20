56e7ffb3cc588cb33c81d9a6cbacf62efa49d6be
Vladimir Ivanov <icu0755@gmail.com>
2018-04-20 14:07:37 +0200

    trying to return a jpeg image

--- cmd/apigw/main.go
+++ cmd/apigw/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"image"
+	"image/jpeg"
 )
 
 type Response struct {
@@ -10,12 +14,30 @@ type Response struct {
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	//mw := imagick.NewMagickWand()
+	//defer mw.Destroy()
+	//dw := imagick.NewDrawingWand()
+	//pw := imagick.NewPixelWand()
+
+	//pw.SetColor("white")
+	//dw.Rectangle(0, 0, 100, 100)
+
+	img := image.NewRGBA(image.Rect(0, 0, 100, 50))
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, img, nil)
+	headers := make(map[string]string)
+	headers["Content-Type"] = "image/jpeg"
+
 	return events.APIGatewayProxyResponse{
-		Body: "Go Serverless!",
-		StatusCode: 200,
+		Body:            base64.StdEncoding.EncodeToString(buf.Bytes()),
+		StatusCode:      200,
+		IsBase64Encoded: true,
+		Headers:         headers,
 	}, nil
 }
 
 func main() {
+	//imagick.Initialize()
+	//defer imagick.Terminate()
 	lambda.Start(Handler)
 }
